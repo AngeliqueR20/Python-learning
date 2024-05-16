5c802e26e253ed1d4d21b79d9e458ebff9922dec
AngeliqueR20 <jaquea.r.2012@gmail.com>
2024-05-16 22:28:39 +0800

    Update stream-jantung.py

--- stream-jantung.py
+++ stream-jantung.py
@@ -1,20 +1,17 @@
 import pickle
 import streamlit as st
 
+st.title('Data Mining Prediksi Jantung')
 jantung_model = pickle.load(open('jantung-model.sav', 'rb'))
 
-st.title('Data Mining Prediksi Jantung')
-
-Age = st.text_input ('input nilai Age')
-Sex = st.text_input ('input nilai Sex')
-ChestPainType = st.text_input ('input nilai ChestPainType')
-RestingBP = st.text_input ('input nilai RestingBP')
-Cholesterol = st.text_input ('input nilai Cholesterol')
-FastingBS = st.text_input ('input nilai FastingBS')
-RestingECG = st.text_input ('input nilai RestingECG')
-MaxHR = st.text_input ('input nilai MaxHR')
-ExerciseAngina = st.text_input ('input nilai ExerciseAngina')
-Oldpeak = st.text_input ('input nilai Oldpeak')
-ST_Slope = st.text_input ('input nilai ST_Slope')
-
-jantung_model = pickle.load(open('jantung_model.sav', 'rb'))
+Age = st.text_input('input nilai Age')
+Sex = st.text_input('input nilai Sex')
+ChestPainType = st.text_input('input nilai ChestPainType')
+RestingBP = st.text_input('input nilai RestingBP')
+Cholesterol = st.text_input('input nilai Cholesterol')
+FastingBS = st.text_input('input nilai FastingBS')
+RestingECG = st.text_input('input nilai RestingECG')
+MaxHR = st.text_input('input nilai MaxHR')
+ExerciseAngina = st.text_input('input nilai ExerciseAngina')
+Oldpeak = st.text_input('input nilai Oldpeak')
+ST_Slope = st.text_input('input nilai ST_Slope')
