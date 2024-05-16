9214bede8534e4ad743aa103a6b3a713ade08e78
AngeliqueR20 <jaquea.r.2012@gmail.com>
2024-05-16 22:20:26 +0800

    Update stream-jantung.py

--- stream-jantung.py
+++ stream-jantung.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 
-jantung_model = pickle.load(open(r'C:\Users\USER\jantung\jantung-model.sav', 'rb'))
+jantung_model = pickle.load(open('jantung-model.sav', 'rb'))
 
 st.title('Data Mining Prediksi Jantung')
 
